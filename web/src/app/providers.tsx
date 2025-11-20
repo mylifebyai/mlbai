62737dae3dee0a2fea7e0f90517618d9c5827dcb
mlbai <mylife.byai@gmail.com>
2025-11-20 21:38:05 +1300

    CheckPoint 1

--- web/src/app/providers.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { SupabaseAuthProvider } from "./components/SupabaseAuthProvider";
-
-export function AppProviders({ children }: { children: React.ReactNode }) {
-  return <SupabaseAuthProvider>{children}</SupabaseAuthProvider>;
-}
